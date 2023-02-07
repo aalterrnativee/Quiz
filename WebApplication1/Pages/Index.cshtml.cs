1dbf00cb876e4fd66936be63d1dee24a75d6fec4
aalterrnativee <alternative3004@gmail.com>
2023-02-07 18:37:29 +0330

    Add project files.

---
+++ WebApplication1/Pages/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApplication1.Services;
+
+namespace WebApplication1.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private GetData _getData;
+
+        public IndexModel(ILogger<IndexModel> logger,
+            GetData data)
+        {
+            _logger = logger;
+            _getData = data;
+        }
+
+        public void OnGet()
+        {
+            _getData.FullName();
+        }
+    }
+}
---
+++ WebApplication1/Services/GetData.cs
@@ -0,0 +1,17 @@
+﻿namespace WebApplication1.Services
+{
+    public class GetData
+    {
+        public GetData(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        public string FullName()
+        {
+            return "Alireza Alavi";
+        }
+    }
+}
